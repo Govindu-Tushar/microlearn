591e9a207508a64d5c41804138c6a0c215f62e97
Govindu Tushar <YOUR_EMAIL@gmail.com>
2026-06-04 15:39:21 +0530

    Fix ALLOWED_HOSTS

--- microlearn/settings.py
+++ microlearn/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jzr)io=12r1pjjke*t#!=%_o5m9&o$5^w-nk-0syq_g#nmt*wn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ "micro-learn.onrender.com","localhost","127.0.0.1",]
+
 
 
 # Application definition
