306cfca0f2e81380c5276aa80467f5a834137863
Govindu Tushar <YOUR_EMAIL@gmail.com>
2026-06-05 12:54:53 +0530

    Enable debug for troubleshooting

--- microlearn/settings.py
+++ microlearn/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jzr)io=12r1pjjke*t#!=%_o5m9&o$5^w-nk-0syq_g#nmt*wn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["micro-learn.onrender.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 
